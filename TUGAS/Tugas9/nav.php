3d81448769395d8d2e15a7e4fd7835c718758650
shafa123-N <149360381+shafa123-N@users.noreply.github.com>
2025-06-22 20:22:38 +0700

    Add files via upload

---
+++ TUGAS/Tugas9/nav.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Toko Buku Online</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <div class="container">
+           <a class="navbar-brand" href="/tugas99/index.php">Toko Buku Online</a>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav ms-auto">
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/index.php">Daftar Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/Buku/tambah_buku.php">Tambah Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/Pelanggan/transaksi.php">Buat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/Pelanggan/lihat_transaksi.php">Lihat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/Pelanggan/daftar_pelanggan.php">Daftar Pelanggan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/Pelanggan/tambah_pelanggan.php">Tambah Pelanggan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/tugas99/logout.php">Logout</a>
+                   </li>
+               </ul>
+           </div>
+       </div>
+   </nav>
+   
+   <!-- Tambahkan Bootstrap JS di bawah sebelum penutup body -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
